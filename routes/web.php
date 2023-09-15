8e616369a7c61c9b2a1dd910e17a22ef6057ac23
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-09-15 00:01:41 -0300

    6-Fundamental Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('sobre', function () {
+    return "<h1>Página sobre</h1>";
+})->name('hello');
+
+Route::get('contato', function () {
+    return "<h1>Página Contato</h1>";
+});
+
+Route::get('contato/{id}', function ($id) {
+    return $id;
+})->name('edit-contact');
+
+Route::get('home', function () {
+    return "<a href='".route('edit-contact', 1)."'>Sobre</a>";
+});
+
+/** Route Grouping */
+Route::group(['prefix' => 'customer'], function(){
+    Route::get('/', function () {
+        return "<h1>Lista Customizada</h1>";
+    });
+    
+    Route::get('/create', function () {
+        return "<h1>Lista Customizada Create</h1>";
+    });
+    
+    Route::get('show', function () {
+        return "<h1>Lista Customizada Show</h1>";
+    });
+});
+
+// Route Methods
+/**
+ * GET - Request a resource
+ * POST - Create a new resource
+ * PUT - Update a resource
+ * PATCH - Modify a resource
+ * DELETE - Delete a resource
+ */
+
+/** Fallback Route */
+Route::fallback(function(){
+    return "<h1>Rota não existe!</h1>";
+});
