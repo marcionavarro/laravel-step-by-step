30eebc9f1768617757ec028a980e281bb5850e8a
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-09-15 20:52:44 -0300

    12-Fundamental Database Eloquent ORM

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MyPost;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,13 +14,69 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        Post::withTrashed()->find(5)->forceDelete();
+        //  Post::withTrashed()->find(3)->restore();
+        // return Post::onlyTrashed()->get();
+
+       /*  Post::where('id', 5)->delete();
+        dd('success'); */
+
+        /* $post = Post::where('status', 1)->update([
+                    'status' => 0
+                ]); */
+
+        /* $post = Post::find(5)->update([
+           'title' => 'Atutalizando o post'
+       ]); */
+
+        /* $post = Post::create([
+            'title' => 'Nova inseração assign',
+            'description' => 'Novo Post descrição',
+            'status' => 1,
+            'publish_date' => date('Y-m-d'),
+            'user_id' => 1,
+            'category_id' => 2,
+            'views' => 500
+        ]);        
+        dd('success');
+        */
+
+        /* $post = Post::where('id', 4)->delete();
+        dd('success'); */
+
+        /* $post = Post::find('id', 4)->first(); //collection
+        $post->title = 'Novo Post 8';
+        $post->description = 'Novo Post descrição';
+        $post->save();
+        dd('success'); */
+
+        /* $post = new Post();
+        $post->title = 'Post 4';
+        $post->description = 'Post descrição';
+        $post->status = 1;
+        $post->publish_date = date('Y-m-d');
+        $post->user_id = 1;
+        $post->category_id = 1;
+        $post->views = 400;
+        $post->save();
+        dd('success'); */
+
+        // return Post::where('views', '>' , 100)->orWhere('id', '=', '2')->get();
+        // return Post::where('views', '>' , 100)->where('id', '=', '2')->get();
+
+        /* $posts = Post::all();
+        foreach($posts as $post){
+            echo $post->title;
+        } */
+
         /**
-         * count()
-         * max()
-         * min()
-         * avg()
-         * sum()
+         * 1.Retriveing all data
+         * 2.Retriving single data
+         * 3.Retring a single data or throw error
          */
-        return DB::table('posts')->avg('views');
+        // return Post::all(); // DB::table('posts')->get()
+
+        // return $post = Post::findOrFail(6);
+        // return $posts = MyPost::all();
     }
 }
---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    // fillable
+    // guarded
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'publish_date',
+        'user_id',
+        'category_id',
+        'views'
+    ];
+
+    // protected $guarded = []
+}
